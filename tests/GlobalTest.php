b5b1469667a38e890e7dcd9a07e9ba69d490be25
mschop <m.schophaus@adcada.de>
2019-08-28 20:48:38 +0200

    Add test for debug mode in global mode

--- tests/GlobalTest.php
+++ tests/GlobalTest.php
@@ -14,5 +14,9 @@ class GlobalTest extends TestCase
         $noTeePHP = new NodeFactory(new DefaultEscaper('utf-8'), new UriValidator());
         require __DIR__ . '/../global.php';
         $this->assertEquals('<a></a>', _a());
+
+        $noTeePHP = new NodeFactory(new DefaultEscaper('utf-8'), new UriValidator(), true);
+        $a = $noTeePHP->a();
+        $this->assertEquals(__FILE__ . ':19', $a->getAttributes()['data-source']);
     }
 }
